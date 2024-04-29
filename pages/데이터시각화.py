37ade6f7382a3fcb91f4a2b7816a9ac1a46162ae
70sempoby <167431449+70sempoby@users.noreply.github.com>
2024-04-29 17:18:54 +0000

    fix : 파일 uploader 예외처리

--- pages/데이터시각화.py
+++ pages/데이터시각화.py
@@ -23,14 +23,15 @@ import seaborn as sns
 
 # 입력한 데이터 파일의 특정 열에 대해 그래프 그리기
 csv_file = st.file_uploader(label="데이터 파일을 업로드하세요. 단, CSV파일만 가능합니다.")
+
 if csv_file is not None :
     csv_file_df = pd.read_csv(csv_file, encoding= "euc-kr")
     st.write(csv_file_df.head())
 
-column = st.radio(label="열 이름을 선택해주세요.", options = csv_file_df.columns)
-st.subheader(column, "의 분포 시각화", divider='rainbow')
-st.bar_chart(csv_file_df[column].value_counts())
+    column = st.radio(label="열 이름을 선택해주세요.", options = csv_file_df.columns)
+    st.subheader(column, "의 분포 시각화", divider='rainbow')
+    st.bar_chart(csv_file_df[column].value_counts())
 
-fig, ax = plt.subplots()
-sns.histplot(csv_file_df[column], binrange=[2500, 7000], binwidth=100)
-st.pyplot(fig)
+    fig, ax = plt.subplots()
+    sns.histplot(csv_file_df[column], binrange=[2500, 7000], binwidth=100)
+    st.pyplot(fig)
